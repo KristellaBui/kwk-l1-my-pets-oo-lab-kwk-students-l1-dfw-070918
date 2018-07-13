42f8c737a86975fe9edf0289fcd618c35ed0e05f
KristellaBui <kristellabui@gmail.com>
2018-07-13 18:23:34 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,13 @@
 class Cat
   
-  def initialize(name)
+  def initialize(name, mood)
    @name = name
+   @mood = mood
   end
   
   attr_reader :name
+  attr_accessor :mood
 
 end
 
-cat = Cat.new("crookshanks")
+cat = Cat.new("crookshanks", "nervous")
